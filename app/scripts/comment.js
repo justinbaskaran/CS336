4907ab886ac2a955f9e158bd5ca5cf4da9154e8f
justinbaskaran <justinbaskaran5@gmail.com>
2018-11-10 15:33:47 -0500

    lab11

---
+++ app/scripts/comment.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Remarkable from 'remarkable';
+import '../css/base.css';
+
+
+module.exports = React.createClass({
+  rawMarkup: function() {
+    var md = new Remarkable();
+    //console.log("1" == 1);
+    var rawMarkup = md.render(this.props.children.toString());
+    return { __html: rawMarkup };
+  },
+
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+
+        </h2>
+        <span dangerouslySetInnerHTML={this.rawMarkup()} />
+      </div>
+    );
+  }
+});
+
+
