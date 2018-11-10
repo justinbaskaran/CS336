89653923c429e0e66144f74d32c257a74d953f0a
justinbaskaran <justinbaskaran5@gmail.com>
2018-11-10 15:33:47 -0500

    lab11

---
+++ lab11/app/scripts/commentlist.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../css/base.css';
+
+
+import Comment from './comment.js';
+
+
+module.exports = React.createClass({
+  render: function() {
+    var commentNodes = this.props.data.map(function(comment) {
+      return (
+        <Comment author={comment.author} key={comment.id}>
+          {comment.text}
+        </Comment>
+      );
+    });
+    return (
+      <div className="commentList">
+        {commentNodes}
+      </div>
+    );
+  }});
+
